b72c4efabda7e26f48922f81af86cba574b8fdb4
rahultumpala <rhltumpala@gmail.com>
2024-01-18 14:56:12 +0530

    Subarray Sums I

---
+++ Subarray_Sums_I.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define forn(i, k, e) for (int i = k; i < e; i++)
+
+int32_t main() {
+    int n, x;
+    cin >> n >> x;
+    vector<int> nums(n);
+    forn(i, 0, n) cin >> nums[i];
+    map<int, int> pref;
+    pref[0] = 1;
+    int count = 0;
+    int sum = 0, r = 0;
+    while (r < n) {
+        sum += nums[r++];
+        pref[sum]++;
+        int d = sum - x;
+        pref[sum] = 1;
+        count += pref[d];
+    }
+    cout << count << endl;
+}
